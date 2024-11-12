c7170f2459cdfaceabcd1594f9d14902bd4626eb
Noel <NoelTraperoPastor@hotmail.com.>
2024-11-12 14:48:18 +0100

    validar antes de fichar

--- main.py
+++ main.py
@@ -51,19 +51,48 @@ def fichar(hora , entradaSalida):
     driver.find_element(By.ID, "Dropdown_incidencias2").click()
     option = driver.find_element(By.XPATH, "//option[@value='85']")
     option.click()
+    time.sleep(1)
 
     input_element = driver.find_element(By.ID, "TextBox_desde")
     input_element.clear()
     input_element.send_keys(hora)
+    time.sleep(1)
 
      # Localiza el campo textarea por su ID
     textarea_element = driver.find_element(By.ID, "TextBox_Observaciones")
     textarea_element.send_keys(entradaSalida)
+    time.sleep(1)
 
     # Localiza el botón "Solicitar" usando su ID
     time.sleep(1)
     boton_solicitar = driver.find_element(By.ID, "btnSolicitar").click()
-    time.sleep(2)
+    time.sleep(3)
+
+def validarFichar(horaparam , entradaSalida):
+    try:
+        table_wrapper = driver.find_element(By.ID, 'DataGrid_solicitudes_wrapper')
+        table = table_wrapper.find_element(By.TAG_NAME, 'table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+    except Exception as e:
+        print("No hay solicitudes")
+        return 1; #hay que fichar
+
+    # Iterar a través de todas las filas encontradas
+    for row in rows:
+        # Obtener la hora en la columna 6 (índice 5) y las observaciones en la columna 9 (índice 8)
+        cells = row.find_elements(By.TAG_NAME, 'td')
+        
+        if len(cells) >= 9:  # Verificar que la fila tenga suficientes celdas
+            hora = cells[5].text.strip()  # Columna 6 (hora)
+            observaciones = cells[8].text.strip()  # Columna 9 (observaciones)
+            
+            # Verificar si la fila contiene los valores buscados
+            if hora == horaparam and observaciones == entradaSalida:
+                print("Fila encontrada con hora "+hora+" y observaciones "+entradaSalida)
+                return 0; # NO hay que fichar
+    else:
+        print("No se encontró ninguna fila con la condición especificada.")
+        return 1; #hay que fichar
     
 
 if ficharHoyIncidencias==1:
@@ -88,19 +117,24 @@ if ficharHoyIncidencias==1:
 
 if ficharDiasPasados == 1:
     for fecha in fechasPasadas:
-        time.sleep(1)
+        time.sleep(0.5)
         driver.get(urlFechasPasadas+fecha)
-        time.sleep(2)
+        time.sleep(1.5)
         driver.find_elements(By.CSS_SELECTOR, 'ul.nav.nav-tabs li')[1].click()    # PULSAR SOLICITUDES
-        time.sleep(1)
+        time.sleep(0.5)
         ## Comprobar si fichar
-        fichar("08:00" , "entrada")
+        if validarFichar("08:00" , "entrada") == 1:
+            fichar("08:00" , "entrada")
         if isViernes(fecha):
-            fichar("14:30" , "salida")
+            if validarFichar("14:30" , "salida") == 1:
+                fichar("14:30" , "salida")
         else:
-            fichar("14:00" , "salida")
-            fichar("15:00" , "entrada")
-            fichar("17:30" , "salida")
+            if validarFichar("14:00" , "salida") == 1:
+                fichar("14:00" , "salida")
+            if validarFichar("15:00" , "entrada") == 1:
+                fichar("15:00" , "entrada")
+            if validarFichar("17:30" , "salida") == 1:
+                fichar("17:30" , "salida")
 
 time.sleep(120)
     
